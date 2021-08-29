4fb6c113839d39091c39e88b45e06bdf907c6452
James Zhong <thebitspud@gmail.com>
2021-08-28 20:50:32 -0700

    Added custom -u flag to esbuild
    + auto-uploads code to the yare server every 15 seconds

--- esbuild.js
+++ esbuild.js
@@ -17,22 +17,23 @@ function input() {
 
 const flags = process.argv.splice(2);
 
-const shouldwatch = flags.includes("-w") || flags.includes("--watch");
-const shouldsync = flags.includes("-s") || flags.includes("--sync");
-const switchacc = flags.includes("-a") || flags.includes("--switch-acc");
+const shouldWatch = flags.includes("-w") || flags.includes("--watch");
+const shouldSync = flags.includes("-s") || flags.includes("--sync");
+const switchAcc = flags.includes("-a") || flags.includes("--switch-acc");
+const autoUpload = flags.includes("-u") || flags.includes("--auto-upload");
 
-const usingts = fs.existsSync(path.join(__dirname, "src/main.ts"));
-const usingjs = fs.existsSync(path.join(__dirname, "src/main.js"));
+const usingTs = fs.existsSync(path.join(__dirname, "src/main.ts"));
+const usingJs = fs.existsSync(path.join(__dirname, "src/main.js"));
 
-if (!usingjs && !usingts) {
-	console.log("You don't have a main file smh");
+if (!usingJs && !usingTs) {
+	console.log("You don't have a main file smh my head");
 	process.exit(0);
 }
 
-let mainfile = usingjs ? "src/main.js" : "src/main.ts";
+let mainFile = usingJs ? "src/main.js" : "src/main.ts";
 
 const esbuildConfig = {
-	entryPoints: [mainfile],
+	entryPoints: [mainFile],
 	bundle: true,
 	minify: false,
 	outfile: "dist/bundle.js",
@@ -45,23 +46,37 @@ let acc = null;
 async function build() {
 	let result = esbuild.buildSync(esbuildConfig);
 
-	let code = fs.readFileSync(esbuildConfig.outfile, "utf-8");
-
-	if (result.errors.length > 0) return console.error("Build failed ):".red.bold);
+	if (result.errors.length > 0) return console.error("Build failed :(".red.bold);
 	else console.log("Built successfully".green.bold);
 
-	if (!shouldsync) return;
+	if (autoUpload) console.log("Auto-upload enabled".green.bold);
+	if (shouldSync) await upload();
+}
+
+/** @type {string[]} */
+const uploadedGames = [];
+let uploadTimer;
+
+async function upload() {
+	let code = fs.readFileSync(esbuildConfig.outfile, "utf-8");
 	let games = await sync.getGames(acc.user_id);
+	games = games.filter((g) => !uploadedGames.includes(g.id));
 	let successful = await sync.sendCode(code, games, acc);
 	if (successful) {
-		console.log(
-			"Uploaded your code to these games:".green.bold,
-			games.map((g) => (g ? `${g.server}/${g.id}` : g))
-		);
+		if (games.length > 0) {
+			console.log(
+				"Uploaded your code to these games:".green.bold,
+				games.map((g) => (g ? `${g.server}/${g.id}` : g))
+			);
+			uploadedGames.push(...games.map((g) => g.id));
+		}
 	} else {
 		console.error("Upload to yare failed.".red.bold);
 	}
+
+	uploadTimer = setTimeout(() => upload(), 15000);
 }
+
 function login() {
 	return new Promise(async (resolve) => {
 		console.log("Log in to yare to enable yare-sync".bold);
@@ -70,7 +85,7 @@ function login() {
 		console.log("Password (SHOWN):");
 		let password = ((await input()) + "").split("\n")[0].split("\r")[0];
 		console.log("Trying to log in as".yellow, username);
-		let acc = sync.login(username, password).catch(async (e) => {
+		let acc = sync.login(username, password).catch(async () => {
 			console.log("Invalid username or password, try again".red.bold);
 			resolve(await login());
 		});
@@ -79,9 +94,9 @@ function login() {
 }
 
 async function main() {
-	if (shouldsync) {
+	if (shouldSync) {
 		let savedSessionFilePath = path.join(os.tmpdir(), "yare-sync-last-session.json");
-		if (fs.existsSync(savedSessionFilePath) && !switchacc) {
+		if (fs.existsSync(savedSessionFilePath) && !switchAcc) {
 			let savedSessionFile = JSON.parse(fs.readFileSync(savedSessionFilePath, "utf-8"));
 			console.log("Found previous session".blue);
 			if (sync.verifySession(savedSessionFile)) {
@@ -98,20 +113,21 @@ async function main() {
 		fs.writeFileSync(savedSessionFilePath, JSON.stringify(acc), "utf-8");
 	}
 
-	if (shouldwatch) {
-		await build();
+	await build();
+
+	if (shouldWatch) {
 		watch(
-			path.dirname(mainfile),
+			path.dirname(mainFile),
 			{
 				recursive: true,
 			},
 			(_, file) => {
 				console.log("File change".yellow, file);
+				uploadedGames.splice(0, uploadedGames.length);
+				clearTimeout(uploadTimer);
 				build();
 			}
 		);
-	} else {
-		await build();
 	}
 }
 
